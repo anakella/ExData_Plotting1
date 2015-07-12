b456ad810e1ed1bf180279999483d06322b7d14a
anakella <anakella@hotmail.com>
2015-07-12 04:19:34 -0400

    Update plot1.R
    update plot1 R code with comments

--- plot1.R
+++ plot1.R
@@ -16,3 +16,4 @@ png("plot1.png",width=480, height=480)
 hist(PCDSubset$Global_active_power, col="red", main="Global Active Power", 
      xlab="Global Active Power (kilowatts)",ylab="Frequency")
 dev.off()
+## anakella (C) 2015
