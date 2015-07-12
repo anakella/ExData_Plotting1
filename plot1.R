b985cedd2ae40431afaaa79436b9f0148630e426
anakella <anakella@hotmail.com>
2015-07-12 03:34:22 -0400

    Course Project 1 Plot 1

--- plot1.R
+++ plot1.R
@@ -1,1 +1,15 @@
+##Create a directory on desktop and set working directory to it to begin the project
+## Set File Path 
+filePath <- "./Project1/household_power_consumption.txt"
 
+## PCD variable reads power consumption data from the file located in file path
+PCD <- read.csv(filepath,header=T, sep=';', na.strings="?")
+## Convert date into date format using as.Date
+PCD$Date <- as.Date(PCD$Date, format="%d/%m/%Y")
+## Selecting subset of data for data betwwen 01/02/2007 and 02/02/2007
+PCDSubset <- subset(PCD, Date >="2007-02-01" & Date <="2007-02-02")
+##Plot using png
+png("plot1.png",width=480, height=480)
+## Histogram for Global Active Power -- Plot 1
+hist(PCDSubset$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)",ylab="Frequency")
+dev.off()
