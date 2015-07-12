f2ee9db8be930e729e3590dacbd3e44c2d60b447
anakella <anakella@hotmail.com>
2015-07-12 03:37:20 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,12 +4,15 @@ filePath <- "./Project1/household_power_consumption.txt"
 
 ## PCD variable reads power consumption data from the file located in file path
 PCD <- read.csv(filepath,header=T, sep=';', na.strings="?")
+
 ## Convert date into date format using as.Date
-PCD$Date <- as.Date(PCD$Date, format="%d/%m/%Y")
 ## Selecting subset of data for data betwwen 01/02/2007 and 02/02/2007
+PCD$Date <- as.Date(PCD$Date, format="%d/%m/%Y")
 PCDSubset <- subset(PCD, Date >="2007-02-01" & Date <="2007-02-02")
+
 ##Plot using png
+## Histogram for Global Active Power from the subset of data-- Plot 1
 png("plot1.png",width=480, height=480)
-## Histogram for Global Active Power -- Plot 1
-hist(PCDSubset$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)",ylab="Frequency")
+hist(PCDSubset$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",ylab="Frequency")
 dev.off()
